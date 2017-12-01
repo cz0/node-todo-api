6846dfc7061396f5aec363cf154c1dbb1d7806cb
manko <manko@cngroup.dk>
2017-12-01 15:29:33 +0100

    add GET /todos route

--- server/server.js
+++ server/server.js
@@ -11,7 +11,13 @@ app.use(bodyParser.json())
 app.post('/todos', (req, res) => {
     const todo = new Todo(req.body)
     todo.save().then(
-        doc => res.send(doc), 
+        doc => res.send(doc),
+        err => res.status(400).send(err))
+})
+
+app.get('/todos', (req, res) => {
+    Todo.find().then(
+        todos => res.send({ todos }),
         err => res.status(400).send(err))
 })
 
