80490db04dc6ab0e5bd01259b1f5035c97fb27fd
manko <manko@cngroup.dk>
2017-12-01 15:04:34 +0100

    test POST /todos route

--- server/server.js
+++ server/server.js
@@ -19,6 +19,8 @@ app.listen(3000, () => {
     console.log('Started on port 3000')
 })
 
+module.exports = { app }
+
 
 
 // const newTodo = new Todo({text: 'hi'})
---
+++ server/tests/server.test.js
@@ -0,0 +1,48 @@
+const request = require('supertest')
+
+const { app } = require('../server')
+const { Todo } = require('../models/todo')
+
+beforeEach(
+    done => Todo.remove({}).then(() => done())
+)
+
+describe('POST /todos', () => {
+    test('should create a new todo', (done) => {
+        const text = 'Test todo'
+
+        request(app)
+            .post('/todos')
+            .send({ text })
+            .expect(200)
+            .expect(res => expect(res.body.text).toBe(text))
+            .end((err, res) => {
+                if (err) {
+                    return done(err)
+                }
+
+                Todo.find({}).then(todos => {
+                    expect(todos.length).toBe(1)
+                    expect(todos[todos.length - 1].text).toBe(text)
+                    done()
+                }).catch(e => done(e))
+            })
+    })
+
+    test('should not create an invalid todo', (done) => {
+        request(app)
+            .post('/todos')
+            .send({})
+            .expect(400)
+            .end((err, res) => {
+                if (err) {
+                    return done(err)
+                }
+
+                Todo.find({}).then(todos => {
+                    expect(todos.length).toBe(0)
+                    done()
+                }).catch(e => done(e))
+            })
+    })
+})
