cdf14fe953329fff7aec74a6e744413d846e12cf
manko <manko@cngroup.dk>
2017-12-01 17:17:25 +0100

    add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const { ObjectID } = require('mongodb')
 
 const { mongoose } = require('./db/mongoose')
 const { Todo } = require('./models/todo')
@@ -17,8 +18,25 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
     Todo.find().then(
+
         todos => res.send({ todos }),
         err => res.status(400).send(err))
+})
+
+app.get('/todos/:id', (req, res) => {
+    if (!ObjectID.isValid(req.params.id)) {
+        return res.status(404).send({ msg: 'Wrong id' })
+    }
+
+    Todo.findById(req.params.id)
+        .then(todo => {
+            if (!todo) {
+                res.status(404).send()
+            }
+            res.send({ todo })
+        })
+        .catch(err =>
+            res.status(400).send(err))
 })
 
 app.listen(3000, () => {
@@ -26,14 +44,3 @@ app.listen(3000, () => {
 })
 
 module.exports = { app }
-
-
-
-// const newTodo = new Todo({text: 'hi'})
-
-// newTodo.save().then(doc => {
-//     console.log('Saved todo', doc)
-// }, err => {
-//     console.log('Unable to save todo', err)
-// })
-
