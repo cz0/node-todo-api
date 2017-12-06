d716c71e1428f7b2891fba2dbc154a642fae56de
manko <manko@cngroup.dk>
2017-12-06 06:36:40 +0100

    add POST /users route and token method

---
+++ server/tests/test.js
@@ -0,0 +1,10 @@
+// const articlesWithoutPilot = Object.keys(
+//         articles
+//       ).reduce((curr, next) => {
+//         if (pilotArticles.indexOf(next) < 0) {
+//           return [...curr, articles[next]];
+//         }
+//         return curr;
+//       }, []);
+
+console.log(Object.keys([new Object(), new Object(), new Object()]))
