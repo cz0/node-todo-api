46bbfb0236067843bb31f10ee9015559b50656bf
manko <manko@cngroup.dk>
2017-12-07 22:52:26 +0100

    add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -98,6 +98,16 @@ UserSchema.pre('save', function (next) {
     }
 })
 
+UserSchema.methods.removeToken = function (token) {
+    const user = this
+
+    return user.update({
+        $pull: {
+            tokens: { token }
+        }
+    })
+}
+
 const User = mongoose.model('User', UserSchema)
 
 module.exports = { User }
